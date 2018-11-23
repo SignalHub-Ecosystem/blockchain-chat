b630c5b627a17bf6ab6f6459b36d57642fb1ed2a
Peter David Zafonte <pzafonte@users.noreply.github.com>
2018-11-23 09:22:15 -0500

    the first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    /**
+     * IP Addresses for each validator node
+     */
+    dev: true,
+    peers: [
+        '159.65.168.34', // Devslopes
+        '198.199.65.48'  // Stuart
+    ]
+}
